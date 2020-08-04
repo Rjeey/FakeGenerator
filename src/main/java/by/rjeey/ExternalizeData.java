6bb41cab31c3e8706bc5b0608a0903de81655ffb
Rjeey <licanomega@gmail.com>
2020-08-05 00:34:02 +0300

    Create Flyweight objects

--- src/main/java/by/rjeey/ExternalizeData.java
+++ src/main/java/by/rjeey/ExternalizeData.java
@@ -1,4 +1,8 @@
 package by.rjeey;
 
 public class ExternalizeData {
+    static final int size = 5000000;
+    static String[] fullName = new String[size];
+    static String[] address = new String[size];
+    static String[] phoneNumber = new String[size];
 }
--- src/main/java/by/rjeey/FlyData.java
+++ src/main/java/by/rjeey/FlyData.java
@@ -1,4 +1,17 @@
 package by.rjeey;
 
 public class FlyData {
+
+    private FlyData(){}
+
+    public static String getFullName(int obnum){return null;}
+    public static void setFullName(String fullName, int obnum){}
+    public static String getAddress(int obnum){return null;}
+    public static void setAddress(String address, int obnum){}
+    public static String getPhoneNumber(int obnum){return null;}
+    public static void setPhoneNumber(String phoneNumber, int obnum){}
+
+    public static String str(int obnum){
+        return "";
+    }
 }
