a76513b05ac35cf6ad6a6b29ea854a842ae35c00
Rjeey <licanomega@gmail.com>
2020-08-07 18:25:59 +0300

    removing the extra genration random numb

--- src/main/java/by/rjeey/Generator.java
+++ src/main/java/by/rjeey/Generator.java
@@ -37,33 +37,34 @@ class Generator {
             default -> throw new IllegalStateException("Unexpected value: " + country);
         };
 
+        if (mistakes < 1)
+            mistakes *= recordsCount;
+
         for (int i = 0; i < recordsCount; ++i) {
             name = faker.name().name();
             spaces = name.length() - name.replaceAll(" ", "").length();
             sb.append(name);
 
-            if (spaces != 2) {
+            if (spaces != 2)
                 sb.append(" ").append(faker.name().lastName());
-            }
+
             sb.append("; ")
                     .append(faker.address().zipCode()).append(", ")
                     .append(nameCountry)
                     .append(faker.address().streetName())
                     .append(country.equals("us") ? " " : " , д.")
-                    .append(faker.random().nextInt(2000))
+                    .append(faker.random().nextInt(1, 2000))
                     .append(country.equals("us") ? ", " : ", кв.")
                     .append(faker.random().nextInt(1, 200)).append(", ")
                     .append(faker.address().state()).append("; ")
                     .append(faker.phoneNumber().phoneNumber());
 
-            if (mistakes != 0) {
-                if (mistakes < 1 && i < mistakes) {
-                    mistakes *= recordsCount;
-                    System.out.println(generateMisses(sb, faker.random().nextInt((int) mistakes), country, faker));
-                }
-                System.out.println(generateMisses(sb, mistakes, country, faker));
-            } else
+            if (mistakes == 0)
                 System.out.println(sb.toString());
+            else if (i < mistakes)
+                System.out.println(generateMisses(sb, faker.random().nextInt((int) mistakes), country, faker));
+            else
+                System.out.println(generateMisses(sb, mistakes, country, faker));
             sb.setLength(0);
         }
     }
@@ -82,20 +83,23 @@ class Generator {
                 "\u041D\u041E\u041F\u0420\u0421\u0422\u0423\u040E\u0424\u0425\u0426\u0427\u0428\u042B\u042C\u042D" +
                 "\u042E\u042F00123456789").toCharArray();
         char x, y, letter;
-        int ch;
+        int ch, rn;
 
         if (changer != null && faker != null && local != null)
             for (int i = 0; i < mistakes; ++i) {
+                rn = (local.equals("us")) ? faker.random().nextInt(us.length)
+                        : (local.equals("ru")) ? faker.random().nextInt(ru.length)
+                        : faker.random().nextInt(by.length);
                 letter = switch (local) {
-                    case "us" -> us[(faker.random().nextInt(us.length))];
-                    case "ru" -> ru[(faker.random().nextInt(ru.length))];
-                    case "by" -> by[(faker.random().nextInt(by.length))];
+                    case "us" -> us[rn];
+                    case "ru" -> ru[rn];
+                    case "by" -> by[rn];
                     default -> throw new IllegalStateException("Unexpected value: " + local);
                 };
                 ch = (faker.random().nextInt(changer.length() - 1));
 
                 if ((faker.random().nextInt(2)) == 1) {
-                    changer.insert(faker.random().nextInt(changer.length() - 1), letter);
+                    changer.insert(ch, letter);
                     changer.deleteCharAt(faker.random().nextInt(changer.length() - 1));
                 } else {
                     x = changer.charAt(ch);
